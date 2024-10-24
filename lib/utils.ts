f5fb98ad516fe2089dbb951a7de046c07665ff57
Teboho-Mokoena <tebohomokoena830@gmail.com>
2024-10-24 11:52:42 +0200

    Implement the complete layout

--- lib/utils.ts
+++ lib/utils.ts
@@ -66,9 +66,9 @@ export const formatDateTime = (dateString: Date) => {
 };
 
 export function formatAmount(amount: number): string {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("tn", {
     style: "currency",
-    currency: "USD",
+    currency: "BWP",
     minimumFractionDigits: 2,
   });
 
